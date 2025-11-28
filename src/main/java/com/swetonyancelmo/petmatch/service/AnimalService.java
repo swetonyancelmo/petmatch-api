5bdb7dc7ca362fb78a1c8479c32cc785b8129095
Swetony <swetonyancelmo@gmail.com>
2025-11-28 15:52:17 -0300

    feat: Finaliza CRUD de animal

--- src/main/java/com/swetonyancelmo/petmatch/service/AnimalService.java
+++ src/main/java/com/swetonyancelmo/petmatch/service/AnimalService.java
@@ -1,11 +1,14 @@
 package com.swetonyancelmo.petmatch.service;
 
+import com.swetonyancelmo.petmatch.exceptions.ResourceNotFoundException;
 import com.swetonyancelmo.petmatch.model.Animal;
 import com.swetonyancelmo.petmatch.model.dto.AnimalDTO;
 import com.swetonyancelmo.petmatch.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class AnimalService {
@@ -24,6 +27,35 @@ public class AnimalService {
         return animalRepository.save(novoAnimal);
     }
 
-    // Terminar CRUD de Animal
+    @Transactional
+    public Animal update(Long id, AnimalDTO.Update dto){
+        Animal animal = animalRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado com o ID: " + id));
+
+        if (dto.nome() != null) animal.setNome(dto.nome());
+        if (dto.sexo() != null) animal.setSexo(dto.sexo());
+        if (dto.tipo() != null) animal.setTipo(dto.tipo());
+        if (dto.adotado() != null) animal.setAdotado(dto.adotado());
+
+        return animalRepository.save(animal);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Animal> find(){
+      return animalRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Animal findById(Long id){
+        return animalRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado com o ID: " + id));
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Animal animalEncontrado = animalRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado com o ID: " + id));
+        animalRepository.delete(animalEncontrado);
+    }
 
 }
