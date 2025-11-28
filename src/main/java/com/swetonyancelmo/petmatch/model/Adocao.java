e4a7012d85dc9f124a08fe178ca603d3481143ad
Swetony <swetonyancelmo@gmail.com>
2025-11-28 09:46:47 -0500

    feat: Adiciona classe Adoção

---
+++ src/main/java/com/swetonyancelmo/petmatch/model/Adocao.java
@@ -0,0 +1,35 @@
+package com.swetonyancelmo.petmatch.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "adocoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Adocao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Animal animal;
+
+    @Column(nullable = false)
+    private Adotante adotante;
+
+    private LocalDate dataAdocao;
+
+    @PrePersist
+    private void prePersist(){
+        if(this.dataAdocao == null){
+            this.dataAdocao = LocalDate.now();
+        }
+    }
+}
