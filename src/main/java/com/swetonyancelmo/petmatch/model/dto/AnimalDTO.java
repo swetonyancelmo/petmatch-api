632d3031222a9805ff0befe5d4f80cb46278ad93
Swetony <swetonyancelmo@gmail.com>
2025-11-28 11:52:29 -0500

    refactor: Refatora AnimalDTO e cria CreateDTO e ResponseDTO

--- src/main/java/com/swetonyancelmo/petmatch/model/dto/AnimalDTO.java
+++ src/main/java/com/swetonyancelmo/petmatch/model/dto/AnimalDTO.java
@@ -1,8 +1,29 @@
 package com.swetonyancelmo.petmatch.model.dto;
 
-public record CreateAnimalDTO(
-        String nome,
-        String sexo,
-        String tipo
-) {
+import com.swetonyancelmo.petmatch.model.Animal;
+import jakarta.validation.constraints.NotBlank;
+
+public class AnimalDTO {
+
+    public record Create(
+
+            @NotBlank(message = "O nome não pode ser nulo.")
+            String nome,
+
+            @NotBlank(message = "O sexo do animal não pode ser nulo.")
+            String sexo,
+
+            @NotBlank(message = "O tipo do animal não pode ser nulo.")
+            String tipo
+    ){}
+
+    public record Response(
+            String nome,
+            String sexo,
+            String tipo
+    ){
+        public Response(Animal animal){
+            this(animal.getNome(), animal.getSexo(), animal.getTipo());
+        }
+    }
 }
