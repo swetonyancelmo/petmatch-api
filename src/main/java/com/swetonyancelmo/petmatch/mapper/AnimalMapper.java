8bf253c7c5738e9f1e7611ab63b506f00a3be7c0
Swetony <swetonyancelmo@gmail.com>
2025-12-18 20:57:15 -0300

    feat: Adiciona Mapper

---
+++ src/main/java/com/swetonyancelmo/petmatch/mapper/AnimalMapper.java
@@ -0,0 +1,23 @@
+package com.swetonyancelmo.petmatch.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.swetonyancelmo.petmatch.dto.Request.CreateAnimalDTO;
+import com.swetonyancelmo.petmatch.dto.Request.UpdateAnimalDTO;
+import com.swetonyancelmo.petmatch.dto.Response.AnimalDTO;
+import com.swetonyancelmo.petmatch.model.Animal;
+
+@Mapper(componentModel = "spring")
+public abstract class AnimalMapper {
+    
+    public static final AnimalMapper INSTANCE = Mappers.getMapper(AnimalMapper.class);
+
+    public abstract Animal toEntity (CreateAnimalDTO dto);
+
+    public abstract Animal toEntity (UpdateAnimalDTO dto);
+
+    public abstract Animal toAnimal (AnimalDTO dto);
+
+    public abstract AnimalDTO tDto (Animal animal);
+}
